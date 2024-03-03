b80ab1ea098fe059f39725755f0965531af130a3
holly coloma <holly.life.15@gmail.com>
2024-03-03 01:40:40 +0000

    Add a separate file for logo from ASCII art and description

---
+++ file_texts.py
@@ -0,0 +1,15 @@
+'''
+Contains all long texts
+'''
+LOGO = '''
+ ___   _____      _      _____ 
+|_ _| |_   _|    / \    |_   _|
+ | |    | |     / _ \     | |  
+ | |    | |    / ___ \    | |  
+|___|   |_|   /_/   \_\   |_|  
+'''
+
+DESCRIPTION = '''
+IT Asset Tracker is a tool designed to keep track of various IT assets in your organization. 
+Mainly includes hardware components such as computers, tablets, mobiles phones, keyboard and mouse.
+'''
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
+from file_texts import LOGO, DESCRIPTION
+import re
+from rich.console import Console
+from rich.table import Table
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,8 +17,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('itat')
 
-addstock = SHEET.worksheet('Add Stock')
+def welcome_screen():
+    """
+    Displays logo made from ASCII art and description of the program
+    """
+    print(LOGO)
+    print(DESCRIPTION)
 
-data = addstock.get_all_values()
 
-print(data)
+if __name__=="__main__":
+    welcome_screen()
+
