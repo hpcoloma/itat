f2059861d667469bb263e659eab5f4f6284a47c7
holly coloma <holly.life.15@gmail.com>
2024-03-03 23:56:28 +0000

    Add redirect function on each admin menu options. Add function to add new stock with validation of date input

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ from file_texts import LOGO, DESCRIPTION
 import re
 from rich.console import Console
 from rich.table import Table
+import os
+from datetime import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -23,6 +25,8 @@ def view_stock():
     """
     Display current inventory listing in a table format
     """
+    clear_screen()
+
     index = 0
     table = Table(title='CURRENT STOCK')
     table.add_column('No.')
@@ -39,7 +43,7 @@ def view_stock():
         index += 1
         table.add_row(str(index), *row)
     
-    console.print(table)
+    console.print(table, justify='center')
     
     admin_menu()
 
@@ -47,8 +51,10 @@ def view_status():
     """
     Display check-out sheet with status of each assigned stocks
     """
+    clear_screen()
+
     index = 0
-    table = Table(title='CURRENT STOCK')
+    table = Table(title='ASSIGNED STOCK')
     table.add_column('No.')
     table.add_column('Check-out Date')
     table.add_column('SKU')
@@ -60,11 +66,58 @@ def view_status():
     data = viewstatus.get_all_values()
     for row in data[1:]:
         index +=1
-        table.add_row(str(index), *row)
+        table.add_row(str(index), *row[:5])
     
-    console.print(table)
+    console.print(table, justify='center')
+    admin_menu()
 
+class StockItem():
+    """
+    Create new stock items base on user input of date, type and quantity
+    """
+    def __init__(self, date, item_type, quantity):
+        self.date = date
+        self.item_type = item_type
+        self.quantity = quantity
 
+    def display_info(self):
+        return f"Date: {self.date}, Type: {self.item_type}, Quantity: {self.quantity}"
+
+def validate_date(date_str):
+    try:
+        datetime.strptime(date_str, '%d/%m/%Y')
+        return True
+    except ValueError:
+        return False
+
+def add_stock():
+    """
+    Checks user input type and quantity
+    if valid adds the data to the spreadsheet
+    """
+    clear_screen()
+    index = 0
+    table = Table(title='')
+    table.add_column('No.')
+    table.add_column('Type')
+    
+    stock_type = SHEET.worksheet('source')
+    data = stock_type.get_all_values()
+    for row in data[1:]:
+        index +=1
+        table.add_row(str(index), *row[:1])
+
+    console.print(table, justify='center')
+
+    while True:
+        date_input = input("Enter date (DD/MM/YYY): ").strip()
+        if not validate_date(date_input):
+            print("Invalid date format. Please enter the date in DD/MM/YYY format.")
+        continue
+
+        item_type_input = input("Enter stock type: ")
+
+    admin_menu()
 
 def welcome_screen():
     """
@@ -72,18 +125,55 @@ def welcome_screen():
     """
     console.print(LOGO, justify='center')
     console.print(DESCRIPTION, justify='center')
+    
+    admin_menu()
 
 
 def admin_menu():
     """
-    Admin menu functions
+    Admin menu functions where input will be validated base on the letter assigned
+    for each menu option
     """
-    console.print('''
-    V - VIEW STOCK    S - VIEW STATUS   A - ADD STOCK   E - EXIT
-    ''', justify='center')
+    console.print("""
+    [bold]
+    C.O.M.M.A.N.D   C.E.N.T.E.R
+        V - VIEW STOCK      A - ADD STOCK    B - BOOK REQUEST  
+         S - VIEW STATUS     E - EDIT STOCK   R - REVIEW REQUEST
+        Q - QUIT
+    """, justify='center')
+
+    while True:
+        admin_menu_input = input("\n").strip().lower()
+        if admin_menu_input not in ("v","s","q","a","e","b","r"):
+            print("Invalid input. Please enter the correct letter.")
+        else:
+            break
+    
+    if admin_menu_input == ("v"):
+        view_stock()
+    elif admin_menu_input == ("s"):
+        view_status()
+    elif admin_menu_input == ("a"):
+        add_stock()
+    elif admin_menu_input == ("e"):
+        welcome_screen()
+    elif admin_menu_input == ("b"):
+        welcome_screen()
+    elif admin_menu_input == ("r"):
+        welcome_screen()
+    elif admin_menu_input == ("q"):
+        clear_screen()
+
+def clear_screen():
+    """
+    Clears the terminal window peior to new content.
+    Recommended to me by my mentor, Matt Bodden
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 
 if __name__=="__main__":
     welcome_screen()
-    view_stock()
-    view_status()
+    #view_stock()
+    #view_status()
